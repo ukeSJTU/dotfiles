ac30840f772cd8b81846462db0c1236871abf905
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2026-08-06 11:19:37 +0800

    ci: add automated configuration checks

--- .config/zsh/plugins/git.plugin.zsh
+++ .config/zsh/plugins/git.plugin.zsh
@@ -30,16 +30,16 @@ function git_develop_branch() {
 # Get the default branch name from common branch names or fallback to remote HEAD
 function git_main_branch() {
   command git rev-parse --git-dir &>/dev/null || return
-  
+
   local remote ref
-  
+
   for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk,mainline,default,stable,master}; do
     if command git show-ref -q --verify $ref; then
       echo ${ref:t}
       return 0
     fi
   done
-  
+
   # Fallback: try to get the default branch from remote HEAD symbolic refs
   for remote in origin upstream; do
     ref=$(command git rev-parse --abbrev-ref $remote/HEAD 2>/dev/null)
